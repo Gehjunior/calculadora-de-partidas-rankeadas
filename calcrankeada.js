87ce4789db05e262c3d297ee19b76ac05980d783
Gehjunior <geraldoluiz93@gmail.com>
2023-11-13 20:06:29 -0300

    Desafio Projeto DIO

--- calcrankeada.js
+++ calcrankeada.js
@@ -1,37 +1,45 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 function calcularNivel() {
-  // Informar os valores para Vitorias/Derrotas
-  let vitorias = parseInt(prompt("Informe a quantidade de vitórias:"));
-  let derrotas = parseInt(prompt("Informe a quantidade de derrotas:"));
+  rl.question('Informe a quantidade de vitórias: ', (vitorias) => {
+    rl.question('Informe a quantidade de derrotas: ', (derrotas) => {
+      vitorias = parseInt(vitorias);
+      derrotas = parseInt(derrotas);
 
-  // Validação dos Dados
-  if (isNaN(vitorias) || isNaN(derrotas)) {
-    console.error("Por favor, informe números válidos.");
-    return;
-  }
+      if (isNaN(vitorias) || isNaN(derrotas)) {
+        console.error("Por favor, informe números válidos.");
+        rl.close();
+        return;
+      }
 
-  // processamento dos valores (vitórias - derrotas)
-  let saldoVitorias = vitorias - derrotas;
+      let saldoVitorias = vitorias - derrotas;
 
-  // Determina o nível com base no saldo de vitórias
-  let nivel;
-  if (vitorias < 10) {
-    nivel = "Ferro";
-  } else if (vitorias >= 11 && vitorias <= 20) {
-    nivel = "Bronze";
-  } else if (vitorias >= 21 && vitorias <= 50) {
-    nivel = "Prata";
-  } else if (vitorias >= 51 && vitorias <= 80) {
-    nivel = "Ouro";
-  } else if (vitorias >= 81 && vitorias <= 90) {
-    nivel = "Diamante";
-  } else if (vitorias >= 91 && vitorias <= 100) {
-    nivel = "Lendário";
-  } else {
-    nivel = "Imortal";
-  }
+      let nivel;
+      if (vitorias < 10) {
+        nivel = "Ferro";
+      } else if (vitorias >= 11 && vitorias <= 20) {
+        nivel = "Bronze";
+      } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+      } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+      } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+      } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+      } else {
+        nivel = "Imortal";
+      }
 
-  // Exibe a mensagem
-  console.log(`O Herói tem um saldo de ${saldoVitorias} está no nível de ${nivel}`);
+      console.log(`O Herói tem um saldo de ${saldoVitorias} está no nível de ${nivel}`);
+      rl.close();
+    });
+  });
 }
 
 // Chama a função para calcular o nível com base nas entradas do usuário
